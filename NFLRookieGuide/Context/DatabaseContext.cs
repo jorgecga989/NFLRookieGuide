c1a7df0bcfe391b909a919b1b6bb94a704893b1d
jorgecga989 <jorge.cga2006@gmail.com>
2024-10-08 12:53:51 +0100

    Add project files.

---
+++ NFLRookieGuide/Context/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NFLRookieGuide.Model;
+
+namespace NFLRookieGuide.Context
+{
+    public class DatabaseContext : IdentityDbContext<User>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            var dbPath = Path.Join(path, "database.db");
+            optionbuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
---
+++ NFLRookieGuide/Model/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NFLRookieGuide.Model
+{
+    public class User : IdentityUser
+    {
+    }
+}
