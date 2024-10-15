bc8bc3fe1047217fd0fe52099cb70070280c3fca
jorgecga989 <jorge.cga2006@gmail.com>
2024-10-15 13:11:19 +0100

    Register works and working in seeder

--- NFLRookieGuide/Context/DatabaseSeeder.cs
+++ NFLRookieGuide/Context/DatabaseSeeder.cs
@@ -1,7 +1,26 @@
-﻿namespace NFLRookieGuide.Context
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NFLRookieGuide.Model;
+
+namespace NFLRookieGuide.Context
 {
     public class DatabaseSeeder
     {
+
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
      
+        public DatabaseSeeder(DatabaseContext context, UserManager<User> userManager)
+        {
+         _context = context;
+         _userManager = userManager;
+        }
+
+        public async Task Seed()
+        {
+            await _context.Database.MigrateAsync();
+            
+            
+        }
     }
 }
