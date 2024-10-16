5591dcc6cbaab57ec1e6b32ee6168674c15fccb3
Jorge Garcia Alcantara <jorge.cga2006@gmail.com>
2024-10-16 22:28:25 +0100

    added team and position to player

--- NFLRookieGuide/Model/Player.cs
+++ NFLRookieGuide/Model/Player.cs
@@ -8,6 +8,8 @@
         public string Description { get; set; }
         public string Photo {  get; set; }
         public int Age { get; set; }
+        public Team Team { get; set; } // Details of team where player plays in will be found here
+        public Position Position { get; set; } 
         public List<RosterPlayer>RosterPlayers { get; set; } //List of rosters where thi player is used
 
 
