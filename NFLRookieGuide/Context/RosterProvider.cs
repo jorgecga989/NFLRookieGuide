81586b1eca8a212bfa3ba3a1bd56fec442858487
jorgecga989 <jorge.cga2006@gmail.com>
2024-11-05 14:46:51 +0000

    work on dropdown menu and rosters

---
+++ NFLRookieGuide/Context/RosterProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NFLRookieGuide.Model;
+
+namespace NFLRookieGuide.Context
+{
+    public class RosterProvider
+    {
+        private readonly DatabaseContext _context;
+
+        public RosterProvider(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Roster>> GetAllRosterAsync()
+        {
+            return await _context.Rosters.OrderBy(roster => roster.Name).ToListAsync();
+        }
+
+        public Roster? GetRoster(int id)
+        {
+            return _context.Rosters.Find(id);
+        }
+    }
+}
