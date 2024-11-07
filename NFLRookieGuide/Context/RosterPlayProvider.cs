2314487307f2578c6582f81d0a578614f082c2e1
jorgecga989 <jorge.cga2006@gmail.com>
2024-11-07 09:33:05 +0000

    I dont knolw

--- NFLRookieGuide/Context/RosterPlayProvider.cs
+++ NFLRookieGuide/Context/RosterPlayProvider.cs
@@ -4,6 +4,8 @@ namespace NFLRookieGuide.Context
 {
     public class RosterPlayProvider
     {
+        private List<Roster> _rosters;
+
         private readonly DatabaseContext _context;
 
         public RosterPlayProvider(DatabaseContext context)
@@ -11,20 +13,24 @@ namespace NFLRookieGuide.Context
             _context = context;
         }
 
+        public void AddPosition(Play position1, Play position2, Play position3, Play position4, Play position5, Play position6, Play position7, Play position8, Play position9, Play position10) {
+            //var roster = _rosters.FirstOrDefault(roster => roster.Position.Id)
+        }
+
         //public async Task AddRosterPlayAsync(/*Cheese cheese, User user, int stars*/)
         //{
-        //    // if the user has already rated this cheese, update the rating else add a new rating
-        //    var rosterplay = _context.RosterPlay.FirstOrDefault(r => r.Cheese == cheese && r.User == user);
-        //    if (rating != null)
-        //    {
-        //        rating.Stars = stars;
-        //    }
-        //    else
-        //    {
-        //        rating = new Rating { Cheese = cheese, User = user, Stars = stars };
-        //        _context.Ratings.Add(rating);
-        //    }
-        //    await _context.SaveChangesAsync();
-        //}
+        //  //if the user has already rated this cheese, update the rating else add a new rating
+        //  var rosterplay = _context.RosterPlay.FirstOrDefault(r => r.Cheese == cheese && r.User == user);
+        //  if (rating != null)
+        //  {
+        //      rating.Stars = stars;
+        //  }
+        //  else
+        //  {
+        //      rating = new Rating { Cheese = cheese, User = user, Stars = stars };
+        //      _context.Ratings.Add(rating);
+        //  }
+        //  await _context.SaveChangesAsync();
+        //  }
     }
 }
