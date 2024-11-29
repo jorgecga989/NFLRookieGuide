c1316e4146d97da85bf680d96a68d6b16b5da953
Jorge Garcia Alcantara <jorge.cga2006@gmail.com>
2024-11-29 23:26:34 +0000

    some changes

--- NFLRookieGuide/Context/RosterPlayProvider.cs
+++ NFLRookieGuide/Context/RosterPlayProvider.cs
@@ -12,30 +12,34 @@ namespace NFLRookieGuide.Context
         {
             _context = context;
         }
-        //public async Task AddRosterPlayAsync(RosterPlay rosterPlay)
-        //{
-        //    _context.RosterPlays.Add(rosterPlay);
-        //    await _context.SaveChangesAsync();
-        //    Console.WriteLine(rosterPlay.SelectedSlots[0]);
-        //    Console.WriteLine("Adding");
-        //}
+        public async Task<List<RosterPlay>> GetAllRosterPlaysAsync()
+        {
+            return await _context.RosterPlays.OrderBy(rosterPlay => rosterPlay.Name).ToListAsync();
+        }
+        public async Task AddRosterPlayAsync(RosterPlay rosterPlay)
+        {
+            _context.RosterPlays.Add(rosterPlay);
+            await _context.SaveChangesAsync();
+            Console.WriteLine(rosterPlay.SelectedSlots[0]);
+            Console.WriteLine("Adding");
+        }
 
 
-        //public async Task UpdateRosterPlayAsync(RosterPlay rosterPlay)
-        //{
-        //    _context.RosterPlays.Update(rosterPlay);
-        //    await _context.SaveChangesAsync();
-        //    Console.WriteLine("Updating");
-        //}
+        public async Task UpdateRosterPlayAsync(RosterPlay rosterPlay)
+        {
+            _context.RosterPlays.Update(rosterPlay);
+            await _context.SaveChangesAsync();
+            Console.WriteLine("Updating");
+        }
 
-        //public async Task DeleteRosterPlayAsync(int id)
-        //{
-        //    var rosterPlay = await _context.RosterPlays.FindAsync(id);
-        //    if (rosterPlay != null)
-        //    {
-        //        _context.RosterPlays.Remove(rosterPlay);
-        //        await _context.SaveChangesAsync();
-        //    }
-        //}
+        public async Task DeleteRosterPlayAsync(int id)
+        {
+            var rosterPlay = await _context.RosterPlays.FindAsync(id);
+            if (rosterPlay != null)
+            {
+                _context.RosterPlays.Remove(rosterPlay);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
